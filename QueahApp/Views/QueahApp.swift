fcd995d3268162ad830c3385c5f075010dfbf22a
Stephen Bensley <76857027+stephenbensley@users.noreply.github.com>
2023-06-13 17:27:59 -0700

    Skeleton of multiplatform app.

---
+++ QueahApp/Views/QueahApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueahApp.swift
+//  Queah
+//
+//  Created by Stephen Bensley on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct QueahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
